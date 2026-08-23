be224cfaf1c94e37bc0b8e0c5dd98a343ae3659a
KrrishSharma12 <krishsharma1842@gmail.com>
2026-08-24 01:14:02 +0530

    Added cookie config

--- backend/config/cookie.config.ts
+++ backend/config/cookie.config.ts
@@ -3,13 +3,13 @@ import type { CookieOptions } from "express";
 export const accessTokenCookieOptions: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
-  maxAge: 15 * 60 * 1000, // 15 minutes
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 15 * 60 * 1000,
 };
 
 export const refreshTokenCookieOptions: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 };
